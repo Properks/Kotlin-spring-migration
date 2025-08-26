e5299758ac5ee6036efe079594664c2f5ec9a73c
Jeongmo Seo <wjdah1402@gmail.com>
2025-08-26 15:37:54 +0900

    feat. Swagger 표시를 위한 로그인 Dummy Controller 생성

--- src/main/kotlin/org/jeongmo/practice/global/security/auth/controller/AuthController.kt
+++ src/main/kotlin/org/jeongmo/practice/global/security/auth/controller/AuthController.kt
@@ -3,6 +3,7 @@ package org.jeongmo.practice.global.security.auth.controller
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.jeongmo.practice.global.security.auth.dto.AuthRequestDTO
 import org.jeongmo.practice.global.security.auth.service.AuthService
+import org.jeongmo.practice.global.security.filter.dto.LoginRequestDTO
 import org.namul.api.payload.response.DefaultResponse
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -19,4 +20,8 @@ class AuthController(
         authService.signUp(request)
         return DefaultResponse.noContent()
     }
+
+    // Swagger 표시를 위한 Dummy controller 실제로는 filter에서 처리 후 컨트롤러로 전송 X
+    @PostMapping("/login")
+    fun login(@RequestBody request: LoginRequestDTO): DefaultResponse<Unit> = DefaultResponse.noContent()
 }
