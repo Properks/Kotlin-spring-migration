10ba03717834a07db114a2125c6e55803f1e23d7
Jeongmo Seo <wjdah1402@gmail.com>
2025-10-07 13:17:47 +0900

    fix. config-server dotenv 사용하지 않는 방식으로 변경 - 작동은 되지만 compile time에서 인식 불가

--- config-server/src/main/kotlin/org/jeongmo/migration/config/server/ConfigServerApplication.kt
+++ config-server/src/main/kotlin/org/jeongmo/migration/config/server/ConfigServerApplication.kt
@@ -1,6 +1,5 @@
 package org.jeongmo.migration.config.server
 
-import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.config.server.EnableConfigServer
@@ -17,11 +16,8 @@ fun main(args: Array<String>) {
 }
 
 fun setPrivateKeyEnv() {
-    val dotenv = Dotenv.configure()
-        .directory("./")
-        .load()
 
-    val base64Key = dotenv["CONFIG_REPOSITORY_PRIVATE_KEY"]
+    val base64Key = System.getenv("CONFIG_REPOSITORY_PRIVATE_KEY") ?: return
     val privateKey= String(Base64.getDecoder().decode(base64Key))
 
     System.setProperty("CONFIG_REPOSITORY_PRIVATE_KEY", privateKey)
