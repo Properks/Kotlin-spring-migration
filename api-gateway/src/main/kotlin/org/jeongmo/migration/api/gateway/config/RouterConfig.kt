f2ff75eb27e5a3a400c0b0b7f3e3ec45637e9104
Jeongmo Seo <wjdah1402@gmail.com>
2025-10-14 16:48:27 +0900

    feat. api-gateway 생성

---
+++ api-gateway/src/main/kotlin/org/jeongmo/migration/api/gateway/config/RouterConfig.kt
@@ -0,0 +1,22 @@
+package org.jeongmo.migration.api.gateway.config
+
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class RouterConfig {
+
+    @Bean
+    fun router(routeLocatorBuilder: RouteLocatorBuilder): RouteLocator {
+        return routeLocatorBuilder.routes().
+            route("health-check") {
+                it
+                    .path("/health-check/**")
+                    .uri("lb://health-check")
+            }
+            .build()
+    }
+
+}
