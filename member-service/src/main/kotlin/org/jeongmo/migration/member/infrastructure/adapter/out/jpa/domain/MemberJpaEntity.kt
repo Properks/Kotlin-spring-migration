c16d4ea88254178a8ca90724795162987c6e7f32
Jeongmo Seo <wjdah1402@gmail.com>
2025-10-25 15:26:21 +0900

    fix. member-service 불필요 메소드 삭제

--- member-service/src/main/kotlin/org/jeongmo/migration/member/infrastructure/adapter/out/jpa/domain/MemberJpaEntity.kt
+++ member-service/src/main/kotlin/org/jeongmo/migration/member/infrastructure/adapter/out/jpa/domain/MemberJpaEntity.kt
@@ -4,7 +4,6 @@ import jakarta.persistence.*
 import org.jeongmo.migration.common.domain.jpa.JpaBaseEntity
 import org.jeongmo.migration.member.domain.enum.ProviderType
 import org.jeongmo.migration.member.domain.enum.Role
-import org.jeongmo.migration.member.domain.model.Member
 import java.time.LocalDateTime
 
 @Entity
@@ -36,19 +35,4 @@ class MemberJpaEntity(
     @Column(name = "deleted_at")
     var deletedAt : LocalDateTime?,
 ): JpaBaseEntity() {
-
-    companion object {
-        fun fromDomain(member: Member): MemberJpaEntity = MemberJpaEntity(
-            id = member.id,
-            username = member.username,
-            password = member.password,
-            providerType = member.providerType,
-            nickname = member.nickname,
-            role = member.role,
-            deletedAt = member.deletedAt,
-        ).apply {
-            this.createdAt = member.createdAt ?: LocalDateTime.now()
-            this.updatedAt = member.updatedAt ?: LocalDateTime.now()
-        }
-    }
 }
