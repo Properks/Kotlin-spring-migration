8e0b8fb1575b1c8d38501cfc8a92495efb970621
Jeongmo Seo <wjdah1402@gmail.com>
2025-11-03 21:04:27 +0900

    feat. api-gateway 라우팅 실패 시 에러 처리로 응답 통일 추가 (투명성 유지)

---
+++ api-gateway/src/main/kotlin/org/jeongmo/migration/api/gateway/security/handler/CustomErrorWebExceptionHandler.kt
@@ -0,0 +1,20 @@
+package org.jeongmo.migration.api.gateway.security.handler
+
+import org.jeongmo.migration.api.gateway.security.util.HttpResponseUtil
+import org.namul.api.payload.code.DefaultResponseErrorCode
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+
+@Component
+@Order(-1)
+class CustomErrorWebExceptionHandler(
+    private val httpResponseUtil: HttpResponseUtil,
+): ErrorWebExceptionHandler {
+
+    override fun handle(exchange: ServerWebExchange, ex: Throwable): Mono<Void> {
+        return httpResponseUtil.writeResponse(exchange, DefaultResponseErrorCode._INTERNAL_SERVER_ERROR, ex.message)
+    }
+}
