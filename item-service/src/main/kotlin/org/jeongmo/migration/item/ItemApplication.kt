b160ff07850df49df60d580b55b65de5b3f93c5c
Jeongmo Seo <wjdah1402@gmail.com>
2025-11-08 16:29:25 +0900

    chore. item-service 모듈 생성

---
+++ item-service/src/main/kotlin/org/jeongmo/migration/item/ItemApplication.kt
@@ -0,0 +1,11 @@
+package org.jeongmo.migration.org.jeongmo.migration.item
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItemApplication
+
+fun main(args: Array<String>) {
+    runApplication<ItemApplication>(*args)
+}
