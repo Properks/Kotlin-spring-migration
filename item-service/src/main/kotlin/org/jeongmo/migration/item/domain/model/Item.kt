c801321cdb256f3b64f3801e0aca4baafe01e179
Jeongmo Seo <wjdah1402@gmail.com>
2025-11-09 21:34:49 +0900

    fix. item-service 도메인 기본값 설정

--- item-service/src/main/kotlin/org/jeongmo/migration/item/domain/model/Item.kt
+++ item-service/src/main/kotlin/org/jeongmo/migration/item/domain/model/Item.kt
@@ -4,13 +4,13 @@ import org.jeongmo.migration.item.domain.enums.ItemStatus
 import java.time.LocalDateTime
 
 class Item(
-    val id: Long,
+    val id: Long = 0L,
     var name: String,
     var price: Long,
-    var discount: Double,
+    var discount: Double = 0.0,
     var itemStatus: ItemStatus,
-    var deletedAt: LocalDateTime?,
-    var score: Double,
+    var deletedAt: LocalDateTime? = null,
+    var score: Double? = null,
 ) {
 
     fun getDiscountPrice(): Long =
