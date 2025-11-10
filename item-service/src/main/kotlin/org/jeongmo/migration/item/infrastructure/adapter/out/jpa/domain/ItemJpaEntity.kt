ce7eaf0cdcef36388bae3f1ad306a84845a25dc2
Jeongmo Seo <wjdah1402@gmail.com>
2025-11-10 15:27:35 +0900

    fix. item-service Soft Delete 적용 및 테이블 이름 변경

--- item-service/src/main/kotlin/org/jeongmo/migration/item/infrastructure/adapter/out/jpa/domain/ItemJpaEntity.kt
+++ item-service/src/main/kotlin/org/jeongmo/migration/item/infrastructure/adapter/out/jpa/domain/ItemJpaEntity.kt
@@ -1,11 +1,16 @@
 package org.jeongmo.migration.item.infrastructure.adapter.out.jpa.domain
 
 import jakarta.persistence.*
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.SQLRestriction
 import org.jeongmo.migration.common.domain.jpa.JpaBaseEntity
 import org.jeongmo.migration.item.domain.enums.ItemStatus
 import java.time.LocalDateTime
 
 @Entity
+@Table(name = "item")
+@SQLDelete(sql = "UPDATE item SET deleted_at = now() WHERE item.item_id = ?")
+@SQLRestriction("deleted_at IS NULL")
 class ItemJpaEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
