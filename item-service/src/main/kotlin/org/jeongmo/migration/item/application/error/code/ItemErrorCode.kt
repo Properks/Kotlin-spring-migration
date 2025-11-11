0395d858742887e936d83bf6b32f463f0118e25c
Jeongmo Seo <wjdah1402@gmail.com>
2025-11-11 15:48:22 +0900

    fix. item-service 에러 코드 변경

--- item-service/src/main/kotlin/org/jeongmo/migration/item/application/error/code/ItemErrorCode.kt
+++ item-service/src/main/kotlin/org/jeongmo/migration/item/application/error/code/ItemErrorCode.kt
@@ -14,7 +14,7 @@ enum class ItemErrorCode(
     NOT_FOUND(HttpStatus.NOT_FOUND, "ITEM_404_1", "상품을 찾지 못했습니다."),
     ALREADY_DELETE(HttpStatus.BAD_REQUEST, "ITEM_400_1", "이미 삭제되었거나 존재하지 않는 상품입니다."),
     FAIL_ITEM_DELETE(HttpStatus.INTERNAL_SERVER_ERROR, "ITEM_500_1", "상품 삭제에 실패했습니다."),
-    INVALID_DOMAIN_DATA(HttpStatus.INTERNAL_SERVER_ERROR, "ITEM_500_2", "상품 정보가 유효하지 않습니다."),
+    INVALID_DOMAIN_DATA(HttpStatus.BAD_REQUEST, "ITEM_400_2", "상품 정보가 유효하지 않습니다."),
     ;
 
     override fun getReason(): ErrorReasonDTO =
