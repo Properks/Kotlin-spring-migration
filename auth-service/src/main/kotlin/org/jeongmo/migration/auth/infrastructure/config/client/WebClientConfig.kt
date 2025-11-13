885825cd9a504b4910c721a278f665cb25986cb2
Jeongmo Seo <wjdah1402@gmail.com>
2025-11-13 14:29:38 +0900

    feat. auth-service trace Id 전파를 위해 ObservationRegistry 추가

--- auth-service/src/main/kotlin/org/jeongmo/migration/auth/infrastructure/config/client/WebClientConfig.kt
+++ auth-service/src/main/kotlin/org/jeongmo/migration/auth/infrastructure/config/client/WebClientConfig.kt
@@ -1,5 +1,6 @@
 package org.jeongmo.migration.auth.infrastructure.config.client
 
+import io.micrometer.observation.ObservationRegistry
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
@@ -9,11 +10,13 @@ import org.springframework.web.reactive.function.client.WebClient
 @Configuration
 class WebClientConfig(
     @Value("\${web-client.base-url.member}") private val baseUrl: String,
+    private val observationRegistry: ObservationRegistry,
 ) {
 
     @Bean
     @LoadBalanced
-    fun loadBalancedWebClientBuilder(): WebClient.Builder = WebClient.builder()
+    fun loadBalancedWebClientBuilder(): WebClient.Builder =
+        WebClient.builder().observationRegistry(observationRegistry)
 
     @Bean
     fun memberWebClient(loadBalancerClientBuilder: WebClient.Builder): WebClient =
