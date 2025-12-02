8e55ca98ef2cd56a4779c3ca63071b698203c5b5
Jeongmo Seo <wjdah1402@gmail.com>
2025-12-02 15:30:45 +0900

    chore. 상품 구매 프로젝트 생성

---
+++ bought-item-service/src/main/kotlin/org/jeongmo/migration/bought/item/BoughtItemApplication.kt
@@ -0,0 +1,11 @@
+package org.jeongmo.migration.bought.item
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoughtItemApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoughtItemApplication>(*args)
+}
