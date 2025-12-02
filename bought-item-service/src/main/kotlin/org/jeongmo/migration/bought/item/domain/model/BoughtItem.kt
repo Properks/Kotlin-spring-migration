47e4f6f517f5642a205e2cf9a9a039abf225946e
Jeongmo Seo <wjdah1402@gmail.com>
2025-12-02 15:51:26 +0900

    fix. bought-item-service 누락된 필드 추가

--- bought-item-service/src/main/kotlin/org/jeongmo/migration/bought/item/domain/model/BoughtItem.kt
+++ bought-item-service/src/main/kotlin/org/jeongmo/migration/bought/item/domain/model/BoughtItem.kt
@@ -1,6 +1,7 @@
 package org.jeongmo.migration.bought.item.domain.model
 
 import org.jeongmo.migration.common.domain.base.BaseDomain
+import org.jeongmo.practice.domain.item.bought.entity.enums.BoughtStatus
 import java.time.LocalDateTime
 
 class BoughtItem(
@@ -8,6 +9,7 @@ class BoughtItem(
     var quantity: Long,
     val memberId: Long,
     val itemId: Long,
+    val boughtStatus: BoughtStatus,
     var deletedAt: LocalDateTime? = null,
     createdAt: LocalDateTime? = null,
     updatedAt: LocalDateTime? = null,
