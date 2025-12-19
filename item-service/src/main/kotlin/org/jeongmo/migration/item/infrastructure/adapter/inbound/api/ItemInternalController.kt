70fe99df08b27ee03ac1907ad7128a6bbf8a870e
Jeongmo Seo <wjdah1402@gmail.com>
2025-12-19 19:17:25 +0900

    feat. item-service 내부 API controller 구현

--- item-service/src/main/kotlin/org/jeongmo/migration/item/infrastructure/adapter/inbound/api/ItemInternalController.kt
+++ item-service/src/main/kotlin/org/jeongmo/migration/item/infrastructure/adapter/inbound/api/ItemInternalController.kt
@@ -26,4 +26,14 @@ class ItemInternalController(
         itemCommandUseCase.decreaseItemCount(itemId);
         return DefaultResponse.noContent()
     }
+
+    /**
+     * 상품 개수 1 증가
+     * 낙관적 락으로 실패 시 재시도, 재시도 횟수 내에 실패 시 500에러
+     */
+    @PatchMapping("/{itemId}/increase-stock")
+    fun increaseCount(@PathVariable("itemId") id: Long): DefaultResponse<Unit> {
+        itemCommandUseCase.increaseItemCount(id)
+        return DefaultResponse.noContent()
+    }
 }
