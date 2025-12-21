65e00a1995cb19f2ef0efe8ffd83b425401c9cf3
Jeongmo Seo <wjdah1402@gmail.com>
2025-12-21 15:35:55 +0900

    feat. common-config 멱등성 키 상수, 에러 생성

---
+++ common-config/src/main/kotlin/org/jeongmo/migration/common/utils/idempotency/IdempotencyErrorCode.kt
@@ -0,0 +1,25 @@
+package org.jeongmo.migration.common.utils.idempotency
+
+import org.namul.api.payload.code.BaseErrorCode
+import org.namul.api.payload.code.dto.ErrorReasonDTO
+import org.namul.api.payload.code.dto.supports.DefaultResponseErrorReasonDTO
+import org.springframework.http.HttpStatus
+
+enum class IdempotencyErrorCode(
+    private val httpStatus: HttpStatus,
+    private val code: String,
+    private val message: String,
+): BaseErrorCode {
+
+    ALREADY_COMPLETE(HttpStatus.CONFLICT, "IDEMPOTENCY_409_1", "이미 처리된 요청입니다."),
+    ALREADY_PROCESSING(HttpStatus.CONFLICT, "IDEMPOTENCY_409_2", "이미 해당 요청을 처리하고 있습니다."),
+    ;
+
+    override fun getReason(): ErrorReasonDTO {
+        return DefaultResponseErrorReasonDTO.builder()
+            .httpStatus(this.httpStatus)
+            .code(this.code)
+            .message(this.message)
+            .build()
+    }
+}
