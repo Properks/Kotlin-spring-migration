c095020379aba8cd825a6668841265d3e1f46385
Jeongmo Seo <wjdah1402@gmail.com>
2025-12-24 22:03:48 +0900

    docs. common-config repository 메서드 설명 수정

--- common-config/src/main/kotlin/org/jeongmo/migration/common/utils/idempotency/IdempotencyKeyRepository.kt
+++ common-config/src/main/kotlin/org/jeongmo/migration/common/utils/idempotency/IdempotencyKeyRepository.kt
@@ -9,7 +9,7 @@ interface IdempotencyKeyRepository {
 
     /**
      * Set status of key which is in repository. But, if key already exist, it will return the status of key. And then if key doesn't exist, it will return null
-     * process a request when returned value is null. if value is not null, request will be denied
+     * Process a request when returned value is null. if value is not null, request will be denied. And this method will be processed atomically for avoiding race condition.
      * @return null or IdempotencyStatus which is stored in repository
      */
     fun setStatusIfAbsent(key: String, status: IdempotencyKeyStatus): IdempotencyKeyStatus?
