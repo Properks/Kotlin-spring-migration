4ca7224684ce93769e9d56435446363e904b3096
Jeongmo Seo <wjdah1402@gmail.com>
2025-12-25 14:24:28 +0900

    typo. bought-item-service 에러 코드 오타 수정

--- bought-item-service/src/main/kotlin/org/jeongmo/migration/bought/item/application/error/code/BoughtItemErrorCode.kt
+++ bought-item-service/src/main/kotlin/org/jeongmo/migration/bought/item/application/error/code/BoughtItemErrorCode.kt
@@ -16,7 +16,7 @@ enum class BoughtItemErrorCode(
     ALREADY_DELETE(HttpStatus.BAD_REQUEST, "BOUGHT_ITEM_400_2", "이미 삭제되었습니다."),
     ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "BOUGHT_ITEM_404_2", "해당 아이템을 찾을 수 없습니다."),
     FAIL_TO_DECREASE_ITEM_COUNT(HttpStatus.INTERNAL_SERVER_ERROR, "BOUGHT_ITEM_500_1", "아이템 수량 감소에 실패했습니다."),
-    FAIL_TO_INCREASE_ITEM_COUNT(HttpStatus.INTERNAL_SERVER_ERROR, "BOUGHT_ITEM_500_1", "아이템 수량 복구₩에 실패했습니다."),
+    FAIL_TO_INCREASE_ITEM_COUNT(HttpStatus.INTERNAL_SERVER_ERROR, "BOUGHT_ITEM_500_1", "아이템 수량 복구에 실패했습니다."),
     INVALID_DATA(HttpStatus.BAD_REQUEST, "BOUGHT_ITEM_400_3", "구매한 상품의 데이터 형식이 유효하지 않습니다."),
     OPTIMISTIC_LOCK_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "BOUGHT_ITEM_500_2", "낙관적 락 충돌로 인한 재시도 횟수를 초과하였습니다.")
     ;
