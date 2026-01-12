aafafb6aebd7576a5c1abaacf2dc8db2b7a8f237
Jeongmo Seo <wjdah1402@gmail.com>
2026-01-12 21:59:50 +0900

    docs. auth-service 인터페이스 문서화

--- auth-service/src/main/kotlin/org/jeongmo/migration/auth/application/port/inbound/AuthCommandUseCase.kt
+++ auth-service/src/main/kotlin/org/jeongmo/migration/auth/application/port/inbound/AuthCommandUseCase.kt
@@ -3,7 +3,27 @@ package org.jeongmo.migration.auth.application.port.inbound
 import org.jeongmo.migration.auth.application.dto.*
 
 interface AuthCommandUseCase {
+
+    /**
+     * 회원 가입 처리 메서드
+     * @param request 회원 가입 양식에 맞는 RequestBody
+     * @throws AuthException 회원 가입에 실패한 경우 해당 에러 발생
+     */
     fun signUp(request: SignUpRequest)
+
+    /**
+     * 로그인 처리 메서드
+     * @param request 로그인 양식에 맞는 RequestBody
+     * @return 로그인 처리 후 필요 데이터 반환(토큰)
+     * @throws AuthException 로그인, 사용자 인증에 실패한 경우 해당 에러 발생
+     */
     fun login(request: LoginRequest): LoginResponse
+
+    /**
+     * 토큰 재발급 메서드
+     * @param request 토큰 재발급에 필요한 RequestBody
+     * @return 재발급된 토큰과 추가 데이터
+     * @throws TokenException 재발급을 위해 전달된 토큰이나 내부 데이터가 유효하지 않은 경우 발생
+     */
     fun reissueToken(request: ReissueTokenRequest): ReissueTokenResponse
 }
--- auth-service/src/main/kotlin/org/jeongmo/migration/auth/application/service/TokenAuthService.kt
+++ auth-service/src/main/kotlin/org/jeongmo/migration/auth/application/service/TokenAuthService.kt
@@ -5,7 +5,26 @@ import org.jeongmo.migration.common.token.domain.model.CustomUserDetails
 
 
 interface TokenAuthService {
+
+    /**
+     * Access Token 생성 메서드
+     * @param userDetails 사용자의 인증 정보
+     * @return 생성된 토큰
+     */
     fun createAccessToken(userDetails: CustomUserDetails): String
+
+    /**
+     * Refresh Token 생성 메서드
+     * @param userDetails 사용자의 인증 정보
+     * @return 생성된 토큰
+     */
     fun createRefreshToken(userDetails: CustomUserDetails): String
+
+    /**
+     * 토큰으로 부터 데이터를 가져오는 메서드
+     * @param token 데이터를 가져올 토큰
+     * @return 토큰으로부터 추출한 데이터
+     * @throws TokenException 전달된 토큰이 유효하지 않거나 토큰의 형식이 미리 정의된 형식이 아닌 경우 발생
+     */
     fun getTokenInfo(token: String): TokenInfoDTO
 }
