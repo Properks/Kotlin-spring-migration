57736ec63ffc768079787dd5e1de8209108d04ee
Jeongmo Seo <wjdah1402@gmail.com>
2026-01-14 14:03:48 +0900

    docs. item-service 인터페이스 문서화

--- item-service/src/main/kotlin/org/jeongmo/migration/item/application/port/inbound/ItemCommandUseCase.kt
+++ item-service/src/main/kotlin/org/jeongmo/migration/item/application/port/inbound/ItemCommandUseCase.kt
@@ -3,9 +3,43 @@ package org.jeongmo.migration.item.application.port.inbound
 import org.jeongmo.migration.item.application.dto.*
 
 interface ItemCommandUseCase {
+
+    /**
+     * 상품 생성 메서드
+     * @param request 상품 생성에 필요한 RequestBody
+     * @return 생성된 상품의 데이터
+     */
     fun createItem(request: CreateItemRequest): CreateItemResponse
+
+    /**
+     * 상품 개수 감소 메서드
+     * @param id 상품 id
+     * @param request 상품 개수 감소에 필요한 RequestBody
+     * @throws ItemException 상품을 찾지 못한 경우
+     */
     fun decreaseItemCount(id: Long, request: DecreaseItemStockRequest)
+
+    /**
+     * 상품 개수 증가 메서드
+     * @param id 상품 id
+     * @param request 상품 개수 증가에 필요한 RequestBody
+     * @throws ItemException 상품을 찾지 못한 경우
+     */
     fun increaseItemCount(id: Long, request: IncreaseItemStockRequest)
+
+    /**
+     * 상품 수정 메서드
+     * @param id 상품 id
+     * @param request 상품 수정에 필요한 RequestBody
+     * @return 수정된 상품 데이터
+     *
+     */
     fun updateItem(id: Long, request: UpdateItemRequest): UpdateItemResponse
+
+    /**
+     * 상품 삭제 메서드
+     * @param id 상품 id
+     * @throws ItemException 상품을 찾지 못하거나 이미 삭제된 경우
+     */
     fun deleteItem(id: Long)
 }
--- item-service/src/main/kotlin/org/jeongmo/migration/item/domain/repository/ItemRepository.kt
+++ item-service/src/main/kotlin/org/jeongmo/migration/item/domain/repository/ItemRepository.kt
@@ -4,8 +4,30 @@ import org.jeongmo.migration.item.domain.model.Item
 
 interface ItemRepository {
 
+    /**
+     * 상품 저장 메서드
+     * @param item 저장할 상품의 도메인 데이터
+     * @return 저장된 item 도메인 데이터
+     */
     fun save(item: Item): Item
+
+    /**
+     * 상품 단일 조회 메서드
+     * @param id 상품 id
+     * @return 찾은 경우 상품 데이터, 못 찾은 경우 null
+     */
     fun findById(id: Long): Item?
+
+    /**
+     * 상품 전체 조회 메서드
+     * @return 상품 데이터 List
+     */
     fun findAll(): List<Item>
+
+    /**
+     * 상품 제거 메서드
+     * @param id 상품 id
+     * @return 성공적으로 삭제한 경우 true, 삭제할 데이터가 없는 경우 false
+     */
     fun deleteById(id: Long): Boolean
 }
