6c674b5483aa8da873b113c960c07dfa760a4c28
Jeongmo Seo <wjdah1402@gmail.com>
2026-01-15 15:58:17 +0900

    docs. auth-service 문서 오타 수정

--- auth-service/src/main/kotlin/org/jeongmo/migration/auth/application/port/out/member/MemberServiceClient.kt
+++ auth-service/src/main/kotlin/org/jeongmo/migration/auth/application/port/out/member/MemberServiceClient.kt
@@ -15,7 +15,7 @@ interface MemberServiceClient {
 
     /**
      * 사용자 도메인과 소통하여 적절한 사용자인지 확인
-     * @param request 사용자 확인에 필요한 RequesyBody
+     * @param request 사용자 확인에 필요한 RequestBody
      * @return 인증 완료된 사용자의 데이터
      * @throws AuthException 사용자 인증에 실패한 경우 해당 에러 발생
      */
