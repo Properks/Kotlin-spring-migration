55eb0a5df10eb5eb8f1eb7341b00c9dfd4163695
Jeongmo Seo <wjdah1402@gmail.com>
2026-01-19 17:05:29 +0900

    fix. item-service yml을 통해 자동 주입되는 RedisConnectionFactory 이용

--- item-service/src/main/kotlin/org/jeongmo/migration/item/infrastructure/config/RedisConfig.kt
+++ item-service/src/main/kotlin/org/jeongmo/migration/item/infrastructure/config/RedisConfig.kt
@@ -1,22 +1,13 @@
 package org.jeongmo.migration.item.infrastructure.config
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.RedisSerializer
 
 @Configuration
-class RedisConfig(
-    @Value("\${spring.data.redis.host}") private val redisHost: String,
-    @Value("\${spring.data.redis.port}") private val redisPort: Int
-
-) {
-
-    @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory = LettuceConnectionFactory(redisHost, redisPort)
+class RedisConfig {
 
     @Bean
     fun idempotencyKeyRedisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
