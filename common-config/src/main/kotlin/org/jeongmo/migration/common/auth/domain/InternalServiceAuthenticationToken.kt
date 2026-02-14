c987015b32ca2acb2193b136b9eb2b170c42d135
Jeongmo Seo <wjdah1402@gmail.com>
2026-02-14 14:32:39 +0900

    fix. common-config 토큰 오버라이딩 수정

--- common-config/src/main/kotlin/org/jeongmo/migration/common/auth/domain/InternalServiceAuthenticationToken.kt
+++ common-config/src/main/kotlin/org/jeongmo/migration/common/auth/domain/InternalServiceAuthenticationToken.kt
@@ -3,10 +3,18 @@ package org.jeongmo.migration.common.auth.domain
 import org.jeongmo.migration.common.enums.member.Role
 import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.util.Assert
 
 class InternalServiceAuthenticationToken: AbstractAuthenticationToken(listOf(SimpleGrantedAuthority("ROLE_${Role.INTERNAL_SERVICE.name}"))) {
 
+    init {
+        super.setAuthenticated(true)
+    }
+
     override fun getPrincipal(): Any = "internal-service"
     override fun getCredentials(): Any? = null
-    override fun isAuthenticated(): Boolean = true
+    override fun setAuthenticated(authenticated: Boolean) {
+        Assert.isTrue(!authenticated, "해당 토큰에서 setAuthenticated(true) 기능을 사용할 수 없습니다. 생성자를 이용해주세요.")
+        super.setAuthenticated(false)
+    }
 }
